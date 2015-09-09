bd80cc7a656920bfe16d324cc80e70e29fa97ae2
simone cesano <simone.cesano@gmail.com>
2015-09-09 08:17:33 +0200

    Unlock files first commit

---
+++ unlock_files/install.sh
@@ -0,0 +1,4 @@
+cd ~/Library/Services/
+curl -L https://github.com/simonecesano/Mac-Automator/blob/master/unlock_files/unlock_files.zip?raw=true > unlock_files.zip
+unzip -o unlock_files.zip 
+rm unlock_files.zip 
