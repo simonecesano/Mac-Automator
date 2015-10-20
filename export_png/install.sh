c33ebafd342e435a3c56c53dab91be22c74724e7
simone cesano <simone.cesano@gmail.com>
2015-10-20 09:52:47 +0200

    Moved from wget to curl

--- export_png/install.sh
+++ export_png/install.sh
@@ -1,4 +1,4 @@
 cd ~/Library/Services/
-wget https://github.com/simonecesano/Mac-Automator/raw/master/export_png/export_pngs.zip
+curl https://github.com/simonecesano/Mac-Automator/raw/master/export_png/export_pngs.zip > export_pngs.zip
 unzip -o export_pngs.zip
 rm export_pngs.zip
